bd20629ccf00f51670a6004f49062b01cb25ae86
pol1801 <126429609+pol1801@users.noreply.github.com>
2025-02-14 20:09:37 -0300

    Fix: cambio BrowserRouter por HashRouter para GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+/* import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; */
 import { ThemeProvider, CssBaseline, Box, GlobalStyles } from '@mui/material';
 import { getTheme } from './themes/theme';
 import Footer from './components/Footer';
